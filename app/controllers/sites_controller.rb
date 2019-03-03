02324aae3c1154e84ee4077c9330e2d63951f793
Apple <apple@Apple-no-MacBook-Air.local>
2019-03-03 15:32:56 +0900

    finished

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,46 @@
+class SitesController < ApplicationController
+  def show
+  	@site = Site.find(params[:id])
+  end
+
+  def index
+  
+  end
+  def new
+  	@site = Site.find(params[:id])
+  end
+
+
+  def new
+  	@site = Site.new
+  	@sites = Site.all
+  end
+
+
+
+  def create
+  	site = Site.new(site_params)
+  	site.save
+  	redirect_to site_path(site.id)
+  end
+
+  def edit
+  	@site = Site.find(params[:id])
+  end
+  def update
+  	site = Site.find(params[:id])
+  	site.update(site_params)
+  	redirect_to site_path(site.id)
+  end
+  def destroy
+  	site = Site.find(params[:id])
+  	site.destroy
+  	redirect_to '/sites/new'
+  end
+
+private
+ def site_params
+ 	params.require(:site).permit(:title, :body)
+ end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+resources :sites
+
+root to: 'sites#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
